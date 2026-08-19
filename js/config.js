7f89b03abf806f171d5a455a7c8421f289267e6c
anassezat9-hash <anass.ezat9@gmail.com>
2026-08-19 14:01:08 +0300

    Load hospital type UI enhancements

--- js/config.js
+++ js/config.js
@@ -1,2 +1,3 @@
 window.SUPABASE_URL="https://zyzyavjwdxprwjutnkgf.supabase.co";
 window.SUPABASE_ANON_KEY="sb_publishable_dE_yRy81Z9iVdx9ccOYCmg_8RVITHqA";
+const s=document.createElement('script');s.src='js/hospital-type.js';document.head.appendChild(s);
